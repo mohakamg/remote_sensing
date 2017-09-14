da0bf6ed2b932e06a2ae863ce59e841b5c13c2a4
mohakamg <mohak.kant1amg@gmail.com>
2017-09-13 22:15:06 -0500

    Modified Views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
+    @organization = current_user.organization
+    @sensors = @organization.sensors
   end
+
 end
